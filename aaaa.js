794a691d15d47e0d61d1a893e467f2ca3227cdca
Eyal Kapon <eyalk@jfrog.com>
2025-09-09 17:45:12 +0300

    Update aaaa.js

--- aaaa.js
+++ aaaa.js
@@ -1,3 +1,11 @@
 # Pattern: internal service tokens with custom format
 JFROG_INTERNAL_SVC_xyz123_abc456_TOKEN_789def
 ARTIFACTORY_WORKER_tk_a1b2c3d4e5f6_v2_prod
+
+#a real secret token
+ghp_1A2b3C4d5E6f7G8h9I0j1K2L3M4N5O6P7Q8R
+
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAIEA1234567890abcdefghijklmnop...
+-----END OPENSSH PRIVATE KEY-----
